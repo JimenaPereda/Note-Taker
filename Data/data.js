ad8f1974b6139ca1cf4a8170b014d65b30d0c783
Jimmy P <jimepereda93@gmail.com>
2020-06-25 15:30:21 -0500

    second

---
+++ Data/data.js
@@ -0,0 +1,15 @@
+var groceryNotes = [
+    {
+        noteTitle: 'Grocery List',
+        List1: 'Bananas',
+        List2: 'Squash Soup',
+        List3:  'Almond Milk',
+        List4:  'Mozarella cheese',
+        List5:  'Pasta',
+        List6:  'onions',
+        List7:  'Oat Bread',
+        List8:  'cCelery',
+    }
+];
+
+module.exports =  groceryNotes;
